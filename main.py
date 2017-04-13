33233d1484253da48edc3713787ab8ed8a8c4c45
Leoltron <leoole5@gmail.com>
2017-04-14 01:30:27 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# !/usr/bin/env python3
+
+from tkinter import *
+
+
+def main():
+    root = Tk()
+    root.title('Pentrix')
+    root.minsize(400, 400)
+    root.geometry('400x400')
+    root.iconbitmap(r'icon.ico')
+    root.resizable(True, True)
+    root.mainloop()
+
+
+main()
